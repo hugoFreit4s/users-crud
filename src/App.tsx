1399bf9b0d947bf37b3e2e1f5e1b712bddb329d4
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-11-22 11:48:42 -0300

    Users can be added and deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css'
 
-type User = { name: string, age: number };
+type User = { id: string, name: string, age: number };
 function App() {
   const [user, setUser] = useState<User>();
   const [userName, setUserName] = useState<string>();
@@ -9,12 +9,16 @@ function App() {
   const [usersList, setUsersList] = useState<Array<User>>([]);
 
   function insertUser(user: User) {
-    if (usersList.length > 0) {
-      fetch("http://localhost:8080/user", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(user) }).then(data => data.json()).then(res => setUsersList(prev => [...prev, res]));
-    } else {
-      fetch("http://localhost:8080/user", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(user) }).then(data => data.json()).then(res => setUsersList(res));
-    }
+    fetch("http://localhost:8080/user", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(user) }).then(data => data.json()).then(res => setUsersList(res));
   }
+
+  function deleteUser(ID: string) {
+    fetch("http://localhost:8080/user", { method: "DELETE", body: ID }).then(data => data.json()).then(res => setUsersList(res));
+  }
+
+  useEffect(() => {
+    console.log(usersList)
+  }, [usersList])
 
   useEffect(() => {
     setUsersList([]);
@@ -31,10 +35,15 @@ function App() {
     <>
       <input type="text" onChange={e => setUserName(e.target.value)} />
       <input type="number" name="user_age_input" id="user_age_input" onChange={e => setUserAge(Number(e.target.value))} />
-      <button onClick={() => insertUser({ name: userName || '', age: userAge || 0 })}>Cadastrar</button>
+      <button onClick={() => insertUser({ id: crypto.randomUUID(), name: userName || '', age: userAge || 0 })}>Cadastrar</button>
       <div style={{ display: 'flex', gap: '20px', color: 'red' }}>
         {usersList.map(user => {
-          return <p>{user.name}</p>
+          return (
+            <div>
+              <p>{user.name}</p>
+              <button onClick={() => deleteUser(user.id)}>del</button>
+            </div>
+          )
         })}
       </div>
     </>
