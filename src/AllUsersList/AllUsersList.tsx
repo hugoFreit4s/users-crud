3c9092ec0cacabb84a2b69c209bdd04c02edba96
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-11-25 13:44:34 -0300

    css change

--- src/AllUsersList/AllUsersList.tsx
+++ src/AllUsersList/AllUsersList.tsx
@@ -1,6 +1,7 @@
 import { User } from "../App";
 import styles from "./AllUsersList.module.css";
 import UserContainerInsideList from "../UserContainerInsideList/UserContainerInsideList";
+import { useEffect, useState } from "react";
 
 type AllUsersListProps = {
     closeModal: () => void;
@@ -10,14 +11,21 @@ type AllUsersListProps = {
 };
 
 export default function AllUsersList({ closeModal, userList, deleteUser, editUser }: AllUsersListProps) {
-
+    const [contentClassName, setContentClassName] = useState<string>("");
+    useEffect(() => {
+        if (userList.length <= 0) {
+            setContentClassName(`${styles.content} ${styles["content-1"]}`)
+        } else {
+            setContentClassName(styles.content)
+        }
+    }, [userList])
     return (
         <div className={styles.backdrop} onClick={() => closeModal()}>
             <div style={{ width: "90%", display: "flex", justifyContent: "end", padding: "0 0 0 0" }}>
                 <div className={styles["close-modal-btn"]} onClick={() => closeModal()}>&#10005;</div>
             </div>
 
-            <div className={styles.content} onClick={e => e.stopPropagation()}>
+            <div className={contentClassName} onClick={e => e.stopPropagation()}>
                 <div className={styles["inner-content"]}>
                     {userList.length > 0 ?
                         userList.map(user => {
