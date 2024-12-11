bf371fa3cd318c1fd33aa391af1a48f0f64cbe65
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-12-11 10:31:02 -0300

    Delete car modal created

--- src/Cars/CarContainer/CarContainer.tsx
+++ src/Cars/CarContainer/CarContainer.tsx
@@ -1,17 +1,15 @@
 import CustomButton from "../../CustomButton/CustomButton";
 import { getCarDTO } from "../CarsPage/CarsPage"
-import { deleteCar } from "../../API";
+import { useState } from "react";
+import DeleteCarModal from "../DeleteCarModal/DeleteCarModal";
 
 type CarContainerProps = {
     car: getCarDTO;
     updateCars: (newCars: getCarDTO[]) => void;
 }
 
-async function callDeleteCar(id: number) {
-    return await deleteCar(id);
-}
-
 export default function CarContainer({ car, updateCars }: CarContainerProps) {
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
     return (
         <div>
             <p>Car: {car.name}</p>
@@ -19,21 +17,17 @@ export default function CarContainer({ car, updateCars }: CarContainerProps) {
             <p>Value: {car.value.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</p>
             <CustomButton
                 className="danger-btn"
-                onClickEvent={() => {
-                    const fetchCars = async () => {
-                        try {
-                            const cars = await callDeleteCar(car.id);
-                            updateCars(cars);
-                        } catch (error) {
-                            console.log(error);
-                        }
-                    }
-                    fetchCars();
-                }
-                }
+                onClickEvent={() => setIsDeleteModalOpen(true)}
                 textContent="Delete"
                 key={car.id}
             />
+            {isDeleteModalOpen &&
+                <DeleteCarModal
+                    car={car}
+                    closeModal={() => setIsDeleteModalOpen(false)}
+                    updateCars={newCars => updateCars(newCars)}
+                    key={crypto.randomUUID()}
+                />}
         </div>
     )
 }
